b0eeeb9509e194d3ccc12c7b2b8ca61446b38048
Emma Konrad <emma.konrad92@gmail.com>
2021-02-05 03:09:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function scuberGreetingForFeet(someValue){
 }
 
 function ternaryCheckCity(isNYC){
-  return (isNYC ? 'Ok, sounds good.' : 'No go.');
+  return (isNYC === 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
 function switchOnCharmFromTip(){
