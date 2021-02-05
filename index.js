04351c05141b4cae014c170bdea6fd810823c07b
Emma Konrad <emma.konrad92@gmail.com>
2021-02-05 03:02:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(someValue){
     return 'This one is on me!';
 } else if (someValue > 2000 && someValue < 2500) {
   return 'I will gladly take your thirty bucks.'
-} else if (somevalue > 2500) {
+} else {
   return 'No can do.'
 }
 
